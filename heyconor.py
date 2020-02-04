fa7a074de389e3d4efdd95c4a28d55acca039f93
Conor Ginty <conorginty@gmail.com>
2020-02-04 14:58:23 +0000

    Changed some stuff

--- heyconor.py
+++ heyconor.py
@@ -1,9 +1,3 @@
 # Conor is modifying Karl's code
 
-print("This is dummy text for Karl's python script that Conor wrote")
-
-print("blahhhh")
-print("Hi there")
-
-print("Hey there")
-print("AAAAAAAAAAAAAAAAAAAA")
+print(3 / 200)
